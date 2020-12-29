2c185df077c23cc94a6bfdf3d2b84588d61e2bca
wilsonh415 <wilsonh415@gmail.com>
2020-12-28 23:30:40 -0800

    added actions

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,13 @@
+import ADD_ARTICLE from '../constants/action-types';
+
 const initialState = {
     articles: []
 };
 
 function rootReducer(state = initialState, action) {
+    if(action.type === ADD_ARTICLE) {
+        state.articles.push(action.payload);
+    }
     return state;
 };
 
